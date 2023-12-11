56aa29fe451a9e9eeb604707cb3e3fe1af328833
MaxSuck <124372839+MaxSuck@users.noreply.github.com>
2023-12-11 03:36:29 -0500

    About everything
    added gui

--- StringTest.py
+++ StringTest.py
@@ -1,18 +1,32 @@
-
-from PIL import Image, ImageDraw
+from PIL import Image, ImageDraw, ImageTk
 import math
+import random
+import turtle
+import tkinter as tk
 
-reference = "your.jpg"
+
+reference = "storm.jpg"  # the image used
+nails = 300 #doesn't do much but sets amount of nails across the circle
+strings = 900 #the higher the darker image will appear and usually above 500 too slow and less impact
+minStringLength = 550  #greatly affects results
+
+references = {0:"storm.jpg",1:"Bertram.jpg",2:"daub.png",3:"smiley.jpg",4:"testCircle.tif",
+              5:"your.jpg",6:"nuhuh.png",7:"fun.png",8:"jamesTest.png",9:"Rosen.jpg"}
+
+#don't change
 imageSize = (1000, 1000)
 imageSize1 = 1000
-nails = 200
-notAgain = []
+image = Image.open(reference)
 
+notAgain = []
 nailsD = {}
+nailsT = {}
 ddPixel = {}
 pixelList = []
 
-image = Image.open(reference)
+nailsToConnect = []
+
+randomOn = False
 
 def imageToSquare(img): #image centered tried to be fancy may just overcomplicated it but I think its elligant
     
@@ -29,7 +43,7 @@ def imageToSquare(img): #image centered tried to be fancy may just overcomplicat
 
     img = img.resize(imageSize)
     img = img.convert("L")
-    #img.show()
+
     return (img)
 
 def imageCircle (img): #create a mask over the image of a circle (does nothing but makes it easier to see also better when looking at pixel values though might skew we'll see)
@@ -43,34 +57,35 @@ def imageCircle (img): #create a mask over the image of a circle (does nothing b
     circleImg.paste(img,mask = mask)
     circleImg.save("TrueReference.jpg")
 
-    #circleImg.show()
     return circleImg
 
 def nailPlacement(): #determines pixel placement of the nails arcosss the circle
     nailPlaced = {}
+    nailPlacedT = {}
 
     for i in range(nails):
-        x = imageSize1/2 * math.cos(2*math.pi * i / nails) + 500
-        y = imageSize1/2 * math.sin(2*math.pi * i / nails) + 500
+        x = max(0, min (imageSize1/2 * math.cos(2*math.pi * i / nails) + 500,999))
+        y = max(0, min (imageSize1/2 * math.sin(2*math.pi * i / nails) + 500,999))
 
+        xt = imageSize1/2 * math.cos(2*math.pi * i / nails) 
+        yt = imageSize1/2 * math.sin(2*math.pi * i / nails)        
+
+        nailPlacedT[i] = (xt), (yt) 
         nailPlaced[i] = int(x), int(y) 
 
-    return nailPlaced
+    return nailPlaced, nailPlacedT
 
 def pixels (): #creates a list of all the pixels values
     pixels = list(img.getdata())
     return pixels
     
-def pixelConverter (): #changes picture to rgb (probably not useful)
-    img = Image.new("RGB", (imageSize))
-    img.putdata(pixels())
-    img.show()
-    
 def lineFinder(startNail): #finds the darkest line from a given point to the rest of the nails
     dark0 = []
-    dark1 = [0]
+    dark1 = [255]
     
-    bestNail = 0
+    bestNail = startNail + 1
+    if bestNail >= nails:
+        bestNail -= nails
     
     for i in range(nails):
         
@@ -81,13 +96,15 @@ def lineFinder(startNail): #finds the darkest line from a given point to the res
 
         for cords in cordsList:  #edits list to add all cords values for one line
             x, y = cords
-            dark0.append(ddPixel[x][y])
+            dark0.append(ddPixel[y][x])
         
         tempDark = dark0 #temp storage
         dark0 = []
 
-        if (startNail,i) not in notAgain and sum(tempDark) / len(tempDark) > sum(dark1) / len(dark1): #hopefully avg darkness over the line of the picture compares it to see which is the best connection
-
+        #hopefully avg darkness over the line of the picture compares it to see which is the best connection 
+        #also makes sure the stringLength is long enough so there isn't (as much) clumping towards darker regions
+        #also removes the possibility of going over those two nails again
+        if len(tempDark) > minStringLength and (startNail,i) not in notAgain and (sum(tempDark) / len(tempDark)) < (sum(dark1) / len(dark1)): 
             dark1 = tempDark
             bestNail = i
     
@@ -99,10 +116,14 @@ def nestPixels (): # creates a *double* dictionary where the cords can be found 
     ddPixelf = {}
     dPixel = {}
 
-    for i in range(1001):
-        for j in range(1001):
-            dPixel[j] = pixelList[i+j]
+    pixelCount = 0
+
+    for i in range(1000):
+        for j in range(1000):
+            dPixel[j] = pixelList[pixelCount]
+            pixelCount += 1
         ddPixelf[i] = dPixel
+        dPixel = {}
     return ddPixelf
 
 def bresenhan (pointA, pointB): # bresenhan line algo, finds cords along a line from (x1,y1) to (x2,y2)
@@ -134,44 +155,150 @@ def bresenhan (pointA, pointB): # bresenhan line algo, finds cords along a line 
     return coOrdinates
     
 def drawLines(strings):
-    
-    img = Image.new("RGB", (imageSize))
-    img.putdata(pixelList)
+    print(nails,strings,minStringLength)
+
+    img = Image.new("RGBA", (imageSize), "white")
+    #img.putdata(pixelList)
     draw = ImageDraw.Draw(img)
 
     start = 0
+    
+    screen = turtle.Screen()
+    screen.screensize(1000,1000)
+    screen.tracer(0)
+
+    t = turtle.Turtle()
+    t.speed(0)
+    t.up()
 
     for i in range (strings):
         startPoint, endPoint = lineFinder(start)
         
-        #print(startPoint, endPoint)
-        #print(nailsD[startPoint], nailsD[endPoint])
-        draw.line([ nailsD[startPoint], nailsD[endPoint] ],fill = (0,0,255), width = 1)
+        x0 , y0 = nailsT[startPoint]
+        x1 , y1 = nailsT[endPoint]
+
+        draw.line([ nailsD[startPoint], nailsD[endPoint] ],fill = (120,120,120,60), width = 1)
+        t.goto(x0,y0 * -1)
+        t.down()
+        t.goto(x1,y1 * -1)
+        t.up()
         
+        screen.update()
+
         notAgain.append((startPoint,endPoint))
         if startPoint != endPoint:
             notAgain.append((endPoint, startPoint))
+        
+        nailsToConnect.append((startPoint,endPoint))
+        
+        '''eather one works fine random probably better but the incrementation looks nice when the picture is developing'''
+        if randomOn:
+            start = random.randint(0,nails-1)
+        else:
+            start += 1
+            if start >= nails:
+                start -= nails
 
-        start = endPoint
     img.show()
+    img.save("endProduct.png")
+    turtle.done
 
-img = imageCircle(imageToSquare(image))
+    return img
 
-nailsD =  nailPlacement()
-pixelList = pixels()
-ddPixel = nestPixels()
+def updateImageD(imageN):
+    index = int(imageN)
+    imageTKO = references.get(index,"blank")
+    labelImage.config(text=f"{imageTKO}")
+    
+    currentDisplay = Image.open(references[index])
+    wTK, hTK = currentDisplay.size
+    scaler = 250/hTK
+    currentD = currentDisplay.resize((int(wTK*scaler), int(hTK*scaler)))
+    new_image = ImageTk.PhotoImage(currentD)
+    imageLabel.config(image=new_image)
+    imageLabel.image = new_image 
 
+    global image
+    image = currentDisplay
 
+def updateNails(aNail):
+    labelNails.config(text=f"Amount of Nails: {aNail}")
+    global nails
+    nails = int(aNail)
 
-drawLines(1000)
+def updateStrings(aString):
+    labelString.config(text=f"Amount of String(s): {aString}")
+    global strings
+    strings = int(aString)
 
-#ddPixelTester()
+def updateStringLength(lString):
+    labelStringL.config(text=f"Min Length of String(s): {lString}")
+    global minStringLength 
+    minStringLength = int(lString)
 
-#print(nestPixels())
-#print(bresenhan((0,0),(500,500)))
+root = tk.Tk()
+root.title("String Art Settings")
 
-#print(nailPlacement(nails,imageSize))
-#pixelTester(pixels(imageCircle(imageToSquare(image(reference),imageSize))))
-#imageToSquare(image(reference))
+imaget = Image.open(references[0])
+widthTK, heightTK = imaget.size
+scalerD = 250/heightTK
+imageTK = imaget.resize((int(widthTK*scalerD),int(heightTK*scalerD)))
+
+imageTK = ImageTk.PhotoImage(imageTK)
+
+imageLabel = tk.Label(root, image=imageTK)
+imageLabel.pack()
+
+imageSlider = tk.Scale(root, from_=0, to=len(references)-1, orient=tk.HORIZONTAL, command=updateImageD, length = 400)
+imageSlider.pack(padx=20, pady=5)
+labelImage = tk.Label(root, text="storm.jpg")
+labelImage.pack(side=tk.TOP, padx=10, pady=5)
+
+nailSlider = tk.Scale(root, from_=50, to=500, orient=tk.HORIZONTAL, command=updateNails, length = 400)
+nailSlider.pack(padx=20, pady=5)
+labelNails = tk.Label(root, text="Amount of Nails: 50")
+labelNails.pack(side=tk.TOP, padx=10, pady=5)
+nailSlider.set(150)
+
+stringsSlider = tk.Scale(root, from_=1, to=10000, orient=tk.HORIZONTAL, command=updateStrings, length = 400)
+stringsSlider.pack(padx=20, pady=5)
+labelString = tk.Label(root, text="Amount of String(s): 1")
+labelString.pack(side=tk.TOP, padx=10, pady=5)
+stringsSlider.set(500)
+
+stringLSlider = tk.Scale(root, from_=0, to=1000, orient=tk.HORIZONTAL, command=updateStringLength, length = 400)
+stringLSlider.pack(padx=20, pady=5)
+labelStringL = tk.Label(root, text="Min Length of String(s): 0")
+labelStringL.pack(side=tk.TOP, padx=10, pady=5)
+stringLSlider.set(150)
+
+def toggle():
+    global randomOn
+    if swv.get():
+        randomOn = True
+    else:
+        randomOn = False
+
+swv = tk.BooleanVar()
+
+switchRan = tk.Checkbutton(root, text="Random Nail Placement", variable=swv, command=toggle)
+switchRan.pack(pady=20)
+
+def commencer():
+    global img, nailsD, nailsT, pixelList, ddPixel
+    img = imageCircle(imageToSquare(image))
+    nailsD , nailsT=  nailPlacement()
+    pixelList = pixels()
+    ddPixel = nestPixels()
+    drawLines(strings)
+
+commence = tk.Button(root, text="COMMENCE", command=lambda: commencer())
+commence.pack(pady=20)
+
+root.mainloop()
+
+print(nailsToConnect)
+
+#drawLines(strings)
     
     
